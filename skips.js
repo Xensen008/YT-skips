7c7098837188f0886307961461f7c652ada1fe13
Xensen008 <arnabjyotikakati008@gmail.com>
2024-04-09 14:50:44 +0530

    files added

---
+++ skips.js
@@ -0,0 +1,18 @@
+function checkForAds(){
+    let adExist = document.getElementsByClassName("ad-interrupting").length > 0;
+    let video = document.getElementsByClassName("html5-main-video")[0];
+
+    if (adExist && video){
+        video.playbackRate = 16;
+        video.muted = true;
+    } else if (video) {
+        video.muted = false;
+    }
+
+    let skipButton = document.querySelector(".ytp-ad-skip-button-modern.ytp-button");
+    if (skipButton){
+        skipButton.click();
+    }
+}
+
+setInterval(checkForAds, 100);
