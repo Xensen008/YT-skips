fae5ce52cafaff420411f1e9e7c37dbd7a8f70c1
Xensen008 <arnabjyotikakati008@gmail.com>
2024-04-09 16:13:10 +0530

    popup changes

--- skips.js
+++ skips.js
@@ -1,10 +1,19 @@
+let isEnabled = true;
+let blockedAds = 0;
+
 function checkForAds(){
+    if (!isEnabled) {
+        return;
+    }
+
     let adExist = document.getElementsByClassName("ad-interrupting").length > 0;
     let video = document.getElementsByClassName("html5-main-video")[0];
 
     if (adExist && video){
         video.playbackRate = 16;
         video.muted = true;
+        blockedAds++;
+        chrome.storage.sync.set({blockedAds: blockedAds});
     } else if (video) {
         video.muted = false;
     }
@@ -16,3 +25,15 @@ function checkForAds(){
 }
 
 setInterval(checkForAds, 100);
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.command == "toggle") {
+        isEnabled = !isEnabled;
+        chrome.storage.sync.set({enabled: isEnabled});
+    }
+});
+
+chrome.storage.sync.get(['enabled', 'blockedAds'], function(data) {
+    isEnabled = data.enabled !== undefined ? data.enabled : true;
+    blockedAds = data.blockedAds || 0;
+});
